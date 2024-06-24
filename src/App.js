9c258a29d9cba339ae56d795bb1cfbfc132b921b
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-24 04:08:56 +0000

    Install bootstrap. Work on header and footer contents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 function App() {
 	return (
-		<div>
-			<header></header>
+		<div className="bg-light d-flex flex-column min-vh-100">
+			<header>
+				<nav className="navbar bg-body-tertiary border-bottom border-2">
+					<div className="container-fluid">
+						<span className="navbar-brand mb-0 h1 fw-bold fs-3 mx-auto">
+							Pomodoro Clock
+						</span>
+					</div>
+				</nav>
+			</header>
+			<main className="flex-grow-1"></main>
+			<footer className="container text-center fw-medium text-secondary mt-auto">
+				<p>
+					&copy; {new Date().getFullYear()} Pomodoro Clock. Created By{" "}
+					<a
+						className="text-primary"
+						href="https://github.com/GDA0"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Gideon D. Adeti
+					</a>
+					. All Rights Reserved.
+				</p>
+			</footer>
 		</div>
 	);
 }
